8535cb58b25833d4b2a051c399edf022824cc250
AlaryCGG <2473582@cgodin.qc.ca>
2026-03-30 10:33:06 -0400

    Motor functions
    - Added failsafe for when code crashes so that robot does not continue last instruction forever
    - Added constants for motor dir so that a motor with different polarity can be configured rapidly
    - Made it so that movement functions now have minimal delay. Action duration is now hanlded separately
    - For testing purposes, added the possibility of inputting commands with duration/angle when the file is run directly

--- Moteur.py
+++ Moteur.py
@@ -15,69 +15,78 @@ GPIO.setup(PIN_VIT_G, GPIO.OUT)
 GPIO.setup(PIN_VIT_D, GPIO.OUT)
 GPIO.setup(PIN_DIR_G, GPIO.OUT)
 
+DIR_HIGH = GPIO.LOW
+DIR_LOW = GPIO.HIGH
+
 def stop():
     GPIO.output(PIN_VIT_G, GPIO.LOW)
     GPIO.output(PIN_VIT_D, GPIO.LOW)
-    GPIO.output(PIN_DIR_G, GPIO.LOW)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
+    GPIO.output(PIN_DIR_G, DIR_LOW)
+    GPIO.output(PIN_DIR_D, DIR_LOW)
 
 def gauche():
     print('Gauche!')
     GPIO.output(PIN_VIT_G, GPIO.HIGH)
     GPIO.output(PIN_VIT_D, GPIO.HIGH)
-    GPIO.output(PIN_DIR_G, GPIO.LOW)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
+    GPIO.output(PIN_DIR_G, DIR_LOW)
+    GPIO.output(PIN_DIR_D, DIR_LOW)
     time.sleep(0.01)
 
 def droite():
     print('Droite!')
     GPIO.output(PIN_VIT_G, GPIO.HIGH)
     GPIO.output(PIN_VIT_D, GPIO.HIGH)
-    GPIO.output(PIN_DIR_G, GPIO.HIGH)
-    GPIO.output(PIN_DIR_D, GPIO.HIGH)
+    GPIO.output(PIN_DIR_G, DIR_HIGH)
+    GPIO.output(PIN_DIR_D, DIR_HIGH)
     time.sleep(0.01)
 
-def enAvant():
+def sleep_angle(angle: int):
+    time.sleep((angle*1.6)/360) # 90o = 0.4
+
+def avant():
     print('En avant!')
     GPIO.output(PIN_VIT_G, GPIO.HIGH)
-    GPIO.output(PIN_VIT_D, GPIO.LOW)
-    GPIO.output(PIN_DIR_G, GPIO.HIGH)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
-    time.sleep(0.1)
-    GPIO.output(PIN_VIT_G, GPIO.HIGH)
     GPIO.output(PIN_VIT_D, GPIO.HIGH)
-    GPIO.output(PIN_DIR_G, GPIO.HIGH)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
-    time.sleep(0.4)
-    GPIO.output(PIN_VIT_G, GPIO.HIGH)
-    GPIO.output(PIN_VIT_D, GPIO.LOW)
-    GPIO.output(PIN_DIR_G, GPIO.HIGH)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
-    time.sleep(0.1)
-    GPIO.output(PIN_VIT_G, GPIO.HIGH)
-    GPIO.output(PIN_VIT_D, GPIO.HIGH)
-    GPIO.output(PIN_DIR_G, GPIO.HIGH)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
-    time.sleep(0.4)
-    GPIO.output(PIN_VIT_G, GPIO.HIGH)
-    GPIO.output(PIN_VIT_D, GPIO.LOW)
-    GPIO.output(PIN_DIR_G, GPIO.HIGH)
-    GPIO.output(PIN_DIR_D, GPIO.LOW)
-    time.sleep(0.1)
+    GPIO.output(PIN_DIR_G, DIR_HIGH)
+    GPIO.output(PIN_DIR_D, DIR_LOW)
+    time.sleep(0.01)
 
-def enArriere():
+def arriere():
     print('En arriere!')
     GPIO.output(PIN_VIT_G, GPIO.HIGH)
     GPIO.output(PIN_VIT_D, GPIO.HIGH)
-    GPIO.output(PIN_DIR_G, GPIO.LOW)
-    GPIO.output(PIN_DIR_D, GPIO.HIGH)
-    time.sleep(1)
+    GPIO.output(PIN_DIR_G, DIR_LOW)
+    GPIO.output(PIN_DIR_D, DIR_HIGH)
+    time.sleep(0.01)
 
-try:
-    while True:
-        gauche()
+def main():
+    try:
+        while True:
+            command = input().split(",")
+            isComandTime = len(command) > 1
+            commandTime = float(command[1]) if isComandTime else None
+            if command[0] == "u":
+                avant()
+                time.sleep(commandTime if commandTime else 1)
+            elif command[0] == "d":
+                arriere()
+                time.sleep(commandTime if commandTime else 1)
+            if command[0] == "l":
+                gauche()
+                sleep_angle(commandTime if commandTime else 45)
+            elif command[0] == "r":
+                droite()
+                sleep_angle(commandTime if commandTime else 45)
+            stop()
+    except KeyboardInterrupt:
         stop()
-except KeyboardInterrupt:
-    pass
+        pass
+    except Exception as e:
+        stop() # empêcher le robot d'avancer à l'infini
+        GPIO.cleanup()
+        raise e
+
+if __name__ == "__main__":
+    main()
 
 GPIO.cleanup() # Défaire le setup des GPIO
